a517c7e30df9b3435f31329050a0d5ce21ac1c4c
Jorge Luiz Cardoso <jorge.luiz@tecsomobi.com.br>
2023-11-22 16:09:08 -0300

    Configuração do mochwesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: false,
+  "chromeWebSecurity": false,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: "https://web.test.tmob.com.br",
     retries: 2,
 
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       return config;
     }
   },
